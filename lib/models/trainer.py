b79fa4bbd7cf7f18aae58b3360aff2a778268d64
Jared Patterson <jpwesley34@gmail.com>
2024-05-27 04:53:27 -0700

    Adds getters and setters for Trainer model.

--- lib/models/trainer.py
+++ lib/models/trainer.py
@@ -23,3 +23,25 @@ class Trainer:
             self._name = name
         else:
             raise ValueError("Please enter a non-empty string for the trainer name")
+        
+    @property
+    def hometown(self):
+        return self._hometown
+    
+    @hometown.setter
+    def hometown(self, hometown):
+        if isinstance(hometown, str) and len(hometown):
+            self._hometown = hometown
+        else:
+            raise ValueError("Please enter a non-empty string for the trainer's hometown")
+        
+    @property
+    def badges(self):
+        return self._badges
+    
+    @badges.setter
+    def badges(self, badges):
+        if isinstance(badges, int) and 0 <= badges <= 8:
+            self._badges = badges
+        else:
+            raise ValueError("Please enter an integer between 0 and 8 for the trainer's badge count")
