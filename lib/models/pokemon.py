0e71892caf811491a20f5cea3d211569824b785b
Jared Patterson <jpwesley34@gmail.com>
2024-05-27 05:15:48 -0700

    Adds getters and setters to Pokemon model.

--- lib/models/pokemon.py
+++ lib/models/pokemon.py
@@ -1,6 +1,7 @@
 import json
 import ipdb
 from models.__init__ import CURSOR, CONN
+from models.trainer import Trainer
 
 with open("lib/kanto.json") as file:
     kanto_list = json.load(file)
@@ -16,6 +17,17 @@ class Pokemon:
         #Pokemon.all.append(self)
 
     @property
+    def nickname(self):
+        return self._nickname
+    
+    @nickname.setter
+    def nickname(self, nickname):
+        if isinstance(nickname, str) and not hasattr(self, "nickname") and len(nickname):
+            self._nickname = nickname
+        else:
+            raise ValueError("Please enter a non-empty string for the Pokemon's nickname")
+
+    @property
     def species(self):
         return self._species
     
@@ -24,5 +36,27 @@ class Pokemon:
         if species not in kanto_list:
             raise Exception("Please enter a Kantonian Pokemon species")
         self._species = species
+
+    @property
+    def level(self):
+        return self._level
+    
+    @level.setter
+    def level(self, level):
+        if isinstance(level, str) and 1 <= level <= 100:
+            self._level = level
+        else:
+            raise ValueError("Please enter an integer between 1 and 100 for the Pokemon's level")
+        
+    @property
+    def trainer(self):
+        return self._trainer
+    
+    @trainer.setter
+    def trainer(self, trainer):
+        if isinstance(trainer, str) and Trainer.find_by_name(trainer):
+            self._trainer = trainer
+        else:
+            raise ValueError("trainer must reference an existing Trainer name in the database")
 
 ipdb.set_trace()
